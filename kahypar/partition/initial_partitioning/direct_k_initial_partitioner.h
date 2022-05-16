0c9d268af177106fd81c773200b4813c6c56bd42
Timo Fritsch <uwmub@student.kit.edu>
2022-05-16 15:13:07 +0200

    Add comment and modify error message

--- kahypar/partition/initial_partitioning/direct_k_initial_partitioner.h
+++ kahypar/partition/initial_partitioning/direct_k_initial_partitioner.h
@@ -4,6 +4,9 @@
 #include "kahypar/partition/initial_partitioning/initial_partitioner_base.h"
 
 namespace kahypar {
+/* ! This class should only ever used, when the graph is contracted down to k vertices during coarsening.
+   ! This class can be used with "i-algo=direct_k"
+*/
 class DirectKInitialPartitioner : public IInitialPartitioner,
                                   private InitialPartitionerBase<DirectKInitialPartitioner> {
   using Base = InitialPartitionerBase<DirectKInitialPartitioner>;
@@ -30,7 +33,7 @@ class DirectKInitialPartitioner : public IInitialPartitioner,
         }
       }
       if (_context.partition.k != current_k) {
-        LOG << "THIS SHOULD NOT HAVE BEEN USED";
+        LOG << "!!! Direct k initial partitioning was used, although there were not k nodes !!!";
         std::exit(1);
       }
     }
