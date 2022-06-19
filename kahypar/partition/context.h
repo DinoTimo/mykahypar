d91a6de9952047fa21e1c906701bfca4fa3d32b5
Timo Fritsch <uwmub@student.kit.edu>
2022-06-19 18:18:07 +0200

    Fix minor info printing bug

--- kahypar/partition/context.h
+++ kahypar/partition/context.h
@@ -252,7 +252,7 @@ inline std::ostream& operator<< (std::ostream& str, const LocalSearchParameters&
     if (params.algorithm == RefinementAlgorithm::balance_approaching_kway_fm_km1) {
       str << "    flow model:                       " << params.fm.flow_model << std::endl;
       str << "    balance convergence speed:        " << params.fm.balance_convergence_speed << std::endl;
-      str << "    balance convergence time:         " << params.flow.max_flow_improvement_iterations << std::endl;
+      str << "    balance convergence time:         " << params.fm.balance_convergence_time << std::endl;
       str << "    max flow improvement iterations:  " << params.flow.max_flow_improvement_iterations << std::endl;
     }
   } else if (params.algorithm == RefinementAlgorithm::do_nothing) {
