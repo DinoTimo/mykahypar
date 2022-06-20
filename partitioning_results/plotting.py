1f9b9c814b48e2938ef80dbaef480cd9f84a6a8a
Timo Fritsch <uwmub@student.kit.edu>
2022-06-20 16:00:04 +0200

    Refactor diagrams, yet still no logarithmic scales..

--- partitioning_results/plotting.py
+++ partitioning_results/plotting.py
@@ -10,35 +10,53 @@ targetpath = path + "target_imbalances.txt"
 resultspath = path + "other_results.txt"
 
 def main():
-  fig, (ax1, ax2) = plt.subplots(2)
-  showImbalance(fig, ax1)
-  showKm1(fig, ax2)
+  plt.figure()
+  plt.subplot(211)
+  showImbalance()
+  plt.subplot(212)
+  showKm1()
   plt.show()
 
 def readLines(path):
   with open(path) as file:
     return file.read().splitlines()
 
+#Expecting the following format for imbalance.txt: one value per line
+#Expecting the following format for target_imbalances.txt: one value per line
+def showImbalance():
+  imbalances = readLines(imbpath)
+  num = len(imbalances)
+  if (num <= 0) : return
+  plt.plot(range(num), floatify(imbalances), 'b', label='actual imbalances')
+  targetImbalances = readLines(targetpath)
+  if (len(targetImbalances) > 0): 
+    plt.plot(range(num), floatify(targetImbalances), 'r', label='target imbalances')
+    plt.hlines(y=float(targetImbalances[num - 1]), xmin=0, xmax=num - 1 , linewidth=1.5, color='g', label='final target weight')
+  plt.legend()
+  infoLines = readLines(infopath)
+  plt.title('       '.join(infoLines))
+
+
 #Expecting the following format: just one value per line, no comma
-def showKm1(fig, ax):
+def showKm1():
   lines = readLines(km1path)
   num = len(lines)
   if num == 0: return
   initkm1 = float(lines[0])
   finalkm1 = float(lines[len(lines) - 1])
-  ax.hlines(y=initkm1, xmin=0, xmax=num - 1 , linewidth=1.5, color='g', label='initial km1: ' + str(initkm1))
-  ax.hlines(y=finalkm1, xmin=0, xmax=num - 1 , linewidth=1.5, color='r', label='final km1: ' + str(finalkm1))
+  plt.hlines(y=initkm1, xmin=0, xmax=num - 1 , linewidth=1.5, color='g', label='initial km1: ' + str(initkm1))
+  plt.hlines(y=finalkm1, xmin=0, xmax=num - 1 , linewidth=1.5, color='r', label='final km1: ' + str(finalkm1))
 
   infoLines = readLines(infopath)
   infoArr = infoLines[0].split(" ")
   graphFile = infoArr[len(infoArr) - 1]
   kahyparkm1 = getKm1FromFile(graphFile)
   if kahyparkm1 != 0:
-    ax.hlines(y=kahyparkm1, xmin=0, xmax=num - 1 , linewidth=1.5, color='y', label='kahypar km1: ' + str(kahyparkm1))
+    plt.hlines(y=kahyparkm1, xmin=0, xmax=num - 1 , linewidth=1.5, color='y', label='kahypar km1: ' + str(kahyparkm1))
     
-  ax.set_title('km1 goal - minimize')
-  ax.plot(floatify(lines), color='b', label='km1')
-  ax.legend()
+  plt.title('km1 goal - minimize')
+  plt.plot(floatify(lines), color='b', label='km1')
+  plt.legend()
 
 
 def getKm1FromFile(graphFileName):
@@ -50,21 +68,6 @@ def getKm1FromFile(graphFileName):
     if lineElements[0] == graphFileName:
       return float(lineElements[1])
   return 0
-
-#Expecting the following format for imbalance.txt: one value per line
-#Expecting the following format for target_imbalances.txt: one value per line
-def showImbalance(fig, ax):
-  imbalances = readLines(imbpath)
-  targetImbalances = readLines(targetpath)
-  num = len(imbalances)
-  if (num <= 0) : return
-  ax.plot(floatify(imbalances), 'b', label='actual imbalances')
-  if (len(targetImbalances) > 0):
-    ax.plot(floatify(targetImbalances), 'r', label='target imbalances')
-    ax.hlines(y=float(targetImbalances[num - 1]), xmin=0, xmax=num - 1 , linewidth=1.5, color='g', label='final target weight')
-  ax.legend()
-  infoLines = readLines(infopath)
-  ax.set_title('       '.join(infoLines))
 
 def floatify(arr):
   return [None if v == '' else float(v) for v in arr]
